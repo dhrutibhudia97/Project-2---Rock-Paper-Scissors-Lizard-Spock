63f3bbd65264d02a40c36b43294b2912609fb3d6
dhrutibhudia97 <dhruti.bhudia@outlook.com>
2022-09-20 13:33:16 +0000

    Add click event listeners to the buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
-console.log("Connected!!")
+//Wait for the DOM to finish loading before running game.
+document.addEventListener("DOMContentLoaded", function() {
+let button = document.getElementsByTagName("button");
+})
+
+document.getElementById("rock").addEventListener("click", function() {
+    console.log("you clicked on rock!") 
+})
+document.getElementById("paper").addEventListener("click", function() {
+    console.log("you clicked on paper!") 
+})
+document.getElementById("scissors").addEventListener("click", function() {
+    console.log("you clicked on scissors!") 
+})
+document.getElementById("lizard").addEventListener("click", function() {
+    console.log("you clicked on lizard!") 
+})
+document.getElementById("spock").addEventListener("click", function() {
+    console.log("you clicked on spock!") 
+})
