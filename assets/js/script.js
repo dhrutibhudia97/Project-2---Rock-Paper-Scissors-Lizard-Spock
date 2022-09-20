efc90f133a2334e48be4f5c1c29b1ec9172f817b
dhrutibhudia97 <dhruti.bhudia@outlook.com>
2022-09-20 14:40:11 +0000

    Add random choice for computer using array index

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,35 @@
 //Wait for the DOM to finish loading before running game.
-document.addEventListener("DOMContentLoaded", function() {
+/*document.addEventListener("DOMContentLoaded", function() {
 let button = document.getElementsByTagName("button");
-})
+}) */
+
+function getComputerChoice() {
+    const choice = ["rock", "paper", "scissors", "lizard", "spock"];
+    const randomChoice = Math.floor(Math.random() * 5);
+    return choice[randomChoice];
+}
+
+function game(userChoice) {
+    const computerChoice = getComputerChoice();
+    console.log("user choice -->" + userChoice);
+    console.log("computer choice -->" + computerChoice);
+}
+
 
 document.getElementById("rock").addEventListener("click", function() {
-    console.log("you clicked on rock!") 
+    game("you clicked on rock!"); 
 })
 document.getElementById("paper").addEventListener("click", function() {
-    console.log("you clicked on paper!") 
+    game("you clicked on paper!"); 
 })
 document.getElementById("scissors").addEventListener("click", function() {
-    console.log("you clicked on scissors!") 
+    game("you clicked on scissors!"); 
 })
 document.getElementById("lizard").addEventListener("click", function() {
-    console.log("you clicked on lizard!") 
+    game("you clicked on lizard!"); 
 })
 document.getElementById("spock").addEventListener("click", function() {
-    console.log("you clicked on spock!") 
+    game("you clicked on spock!"); 
 })
+
+
