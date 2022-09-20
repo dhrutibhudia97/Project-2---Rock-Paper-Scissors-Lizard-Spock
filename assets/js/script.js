a0efbadb90652bfe07708c683c340f51f0de6ccc
dhrutibhudia97 <dhruti.bhudia@outlook.com>
2022-09-20 18:26:36 +0000

    add game function to check userChoice to computerChoice to get results

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,33 +3,72 @@
 let button = document.getElementsByTagName("button");
 }) */
 
+//computer random choice is generated
 function getComputerChoice() {
     const choice = ["rock", "paper", "scissors", "lizard", "spock"];
-    const randomChoice = Math.floor(Math.random() * 5);
+    const randomChoice = [Math.floor(Math.random() * choice.length)];
     return choice[randomChoice];
 }
 
+
 function game(userChoice) {
-    const computerChoice = getComputerChoice();
-    console.log("user choice -->" + userChoice);
-    console.log("computer choice -->" + computerChoice);
+    const computerChoice = getComputerChoice(); 
+    switch (userChoice + computerChoice) {
+        case "scissorspaper":
+        case "scissorslizard":
+        case "paperrock":
+        case "paperspock":
+        case "rockscissors":
+        case "rocklizard":
+        case "lizardpaper":
+        case "lizardspock":
+        case "spockrock":
+        case "spockscissors":
+            console.log("CONGRATS. YOU WIN!");
+            break;
+        case "scissorsspock":
+        case "scissorsrock":
+        case "paperscissors":
+        case "paperlizard":
+        case "rockspock":
+        case "rockpaper":
+        case "lizardscissors":
+        case "lizardrock":
+        case "spockpaper":
+        case "spockslizard":
+            console.log("AWWW. YOU LOSE!");
+            break;
+        case "scissorsscissors":
+        case "paperpaper":
+        case "rockrock":
+        case "lizardlizard":
+        case "spockspock":
+            console.log("IT'S A DRAW.");
+            break; 
+    } 
 }
 
 
-document.getElementById("rock").addEventListener("click", function() {
-    game("you clicked on rock!"); 
-})
-document.getElementById("paper").addEventListener("click", function() {
-    game("you clicked on paper!"); 
-})
-document.getElementById("scissors").addEventListener("click", function() {
-    game("you clicked on scissors!"); 
-})
-document.getElementById("lizard").addEventListener("click", function() {
-    game("you clicked on lizard!"); 
-})
-document.getElementById("spock").addEventListener("click", function() {
-    game("you clicked on spock!"); 
-})
 
 
+
+ //user clicking each option recognised by console.
+function main() {
+    document.getElementById("rock").addEventListener("click", function() {
+        game("rock"); 
+    })
+    document.getElementById("paper").addEventListener("click", function() {
+        game("paper"); 
+    })
+    document.getElementById("scissors").addEventListener("click", function() {
+        game("scissors"); 
+    })
+    document.getElementById("lizard").addEventListener("click", function() {
+        game("lizard"); 
+    })
+    document.getElementById("spock").addEventListener("click", function() {
+        game("lizard"); 
+    })
+}
+
+main();
