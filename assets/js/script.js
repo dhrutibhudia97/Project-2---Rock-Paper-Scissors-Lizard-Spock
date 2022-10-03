a1256a9fa1953ff91c2bad59fed094f885f1e6b6
dhrutibhudia97 <dhruti.bhudia@outlook.com>
2022-10-03 20:22:32 +0000

    Passed javascript through formatter.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,45 @@
-/**Wait for the DOM to finish loading before running game.
- * Code credited to code institutes love maths module.
- */
-document.addEventListener("DOMContentLoaded", function() {
-let button = document.getElementsByTagName("button");
-}) 
 main();
 
+// Constant set for animation duration of the border colour change.
 const _animation_duration = 600;
 
 /**Computer random choice is generated 
  * Code credited to youtube tutorial: "https://www.youtube.com/watch?v=jaVNP3nIAv0".
-*/
-
+ */
 function getComputerChoice() {
     const choice = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
     const randomChoice = [Math.floor(Math.random() * choice.length)];
     return choice[randomChoice];
 }
 
- /**User clicking each button recognised by console.
-  * Code credited to youtube tutorial: "https://www.youtube.com/watch?v=jaVNP3nIAv0".
-  */
- function main() {
+/**User clicking each button recognised by game.
+ * Code credited to youtube tutorial: "https://www.youtube.com/watch?v=jaVNP3nIAv0".
+ */
+function main() {
     document.getElementById("Rock").addEventListener("click", function() {
-        game("Rock"); 
+        game("Rock");
     });
     document.getElementById("Paper").addEventListener("click", function() {
-        game("Paper"); 
+        game("Paper");
     });
     document.getElementById("Scissors").addEventListener("click", function() {
-        game("Scissors"); 
+        game("Scissors");
     });
     document.getElementById("Lizard").addEventListener("click", function() {
-        game("Lizard"); 
+        game("Lizard");
     });
     document.getElementById("Spock").addEventListener("click", function() {
-        game("Spock"); 
+        game("Spock");
     });
-} 
+}
 
 /** 
  * Determines the game output.
- * Switch case used to match the user and computer choice combination and match it to a win/ lose or draw result.
+ * Switch case used to match the user and computer choice combination and match it to a win, lost or draw result.
  * Code credited to the youtube tutorial: "https://www.youtube.com/watch?v=jaVNP3nIAv0&ab_channel=freeCodeCamp.org"
-*/
+ */
 function game(userChoice) {
-    const computerChoice = getComputerChoice(); 
+    const computerChoice = getComputerChoice();
     switch (userChoice + computerChoice) {
         case "ScissorsPaper":
         case "ScissorsLizard":
@@ -65,26 +59,26 @@ function game(userChoice) {
         case "LizardLizard":
         case "SpockSpock":
             draw(userChoice, computerChoice);
-            break; 
-        default: 
+            break;
+        default:
             lost(userChoice, computerChoice);
-    } 
+    }
 }
 
 /**
  * Function won,lost and draw defined above, displays results-text message at the top of the screen.
  * Function "won" adds one to the user score, "lost" adds one to the computer score.
- * Border colour changes for 600 milliseconds depending on game outcome.
+ * @param setTimeout used to set border colour change to 600 milliseconds depending on game outcome.
  * Code credited to the youtube tutorial: "https://www.youtube.com/watch?v=jaVNP3nIAv0" and code institutes love maths module.
- * @param setTimeout
-*/
+ */
 function won(userChoice, computerChoice) {
     incrementUserScore();
-    document.getElementById("result-text").innerHTML = ("You picked " + userChoice + " & the Computer picked " + computerChoice + ". CONGRATS YOU WIN :D");   
+    document.getElementById("result-text").innerHTML = ("You picked " + userChoice + " & the Computer picked " + computerChoice + ". CONGRATS YOU WIN :D");
     document.getElementById(userChoice).classList.add("user-win");
-    
-    setTimeout(function () {document.getElementById(userChoice).classList.remove("user-win");
-      }, _animation_duration);
+
+    setTimeout(function() {
+        document.getElementById(userChoice).classList.remove("user-win");
+    }, _animation_duration);
 }
 
 function lost(userChoice, computerChoice) {
@@ -92,40 +86,39 @@ function lost(userChoice, computerChoice) {
     document.getElementById("result-text").innerHTML = ("You picked " + userChoice + " & the Computer picked " + computerChoice + ". AWW YOU LOSE :(");
     document.getElementById(userChoice).classList.add("user-lose");
 
-    setTimeout(function () {document.getElementById(userChoice).classList.remove("user-lose");
-      }, _animation_duration);
+    setTimeout(function() {
+        document.getElementById(userChoice).classList.remove("user-lose");
+    }, _animation_duration);
 }
 
-function draw (userChoice) {
+function draw(userChoice) {
     document.getElementById("result-text").innerHTML = ("You both picked " + userChoice + ". IT'S A DRAW.");
     document.getElementById(userChoice).classList.add("user-draw");
 
     animateUserSelection("user-draw");
-    setTimeout(function () {document.getElementById(userChoice).classList.remove("user-draw");
+    setTimeout(function() {
+        document.getElementById(userChoice).classList.remove("user-draw");
     }, _animation_duration);
 }
 
 /**Animates the choice selected by user based on whether they won/lost/draw.
  * @param animationClass
  */
-function animateUserSelection (animationClass) {
-    setTimeout(function () {
-      document.getElementById(userChoice).classList.remove(animationClass);
+function animateUserSelection(animationClass) {
+    setTimeout(function() {
+        document.getElementById(userChoice).classList.remove(animationClass);
     }, _animation_duration);
-  } 
+}
 
-/**Functions to increment either user or computer score at the bottom of the page depending on win/loss outcome 
+/**Functions to increment either user or computer score at the bottom of the page depending on win/lost outcome. 
  * Credit to code institutes love maths module.
-*/
-function incrementUserScore () {
+ */
+function incrementUserScore() {
     let oldScore = parseInt(document.getElementById("user-score").innerText);
     document.getElementById("user-score").innerText = ++oldScore;
 }
 
-function incrementComputerScore () {
+function incrementComputerScore() {
     let oldScore = parseInt(document.getElementById("computer-score").innerText);
     document.getElementById("computer-score").innerText = ++oldScore;
 }
-
-
-
